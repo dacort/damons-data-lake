bc59b89eafb035ca26bcb871e86004ee4ca2062f
dacort <d.lifehacker@gmail.com>
2019-05-09 10:41:31 +0100

    Remove boto3 dependency and clean up the code a little. ALso add a new repo. :)

--- glue_deps/setup.py
+++ glue_deps/setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name = "glue_deps",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages(),
     install_requires=[
         'requests==2.21.0',
-        'boto3>1.9',
     ]
 )
--- src/github-stats.py
+++ src/github-stats.py
@@ -1,68 +1,73 @@
 import datetime
 import json
 import sys
-
-# boto3 modules are apparently already present in sys.modules when this script runs.
-# Because we need a new version of boto3 to be able to use secretsmanager, we need to
-# clear out any boto3 and botocore-related modules before we import.
-module_keys = sys.modules.keys()  # Otherwise, we get an error about the dict changing
-for key in module_keys:
-    if key.startswith("boto3") or key.startswith("botocore"):
-        print("Deleting " + key)
-        del sys.modules[key]
-
-
 import os
+
 import boto3
 import requests
 from awsglue.utils import getResolvedOptions
 
-JOB_ARGS = getResolvedOptions(sys.argv, ['sekret', 'bucket', 'prefix'])
+JOB_ARGS = getResolvedOptions(sys.argv, ["sekret", "bucket", "prefix"])
 
-# --------------------------------------------------
-# Helper functions to retrieve environment variables
-# --------------------------------------------------
+"""
+Helper functions to retrieve environment variables
+"""
+
+
 def get_region():
-    return os.environ.get('AWS_DEFAULT_REGION')
+    return os.environ.get("AWS_DEFAULT_REGION")
+
 
 def get_job_arg(name):
     return JOB_ARGS.get(name)
 
+
 def get_secret():
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(
-        'secretsmanager',
-        region_name=get_region()
-    )
+    client = session.client("secretsmanager", region_name=get_region())
 
+    get_secret_value_response = client.get_secret_value(SecretId=get_job_arg("sekret"))
 
-    get_secret_value_response = client.get_secret_value(
-        SecretId=get_job_arg('sekret')
-    )
+    return json.loads(get_secret_value_response["SecretString"])["github_pat"]
 
-    return json.loads(get_secret_value_response['SecretString'])['github_pat']
 
 def save_results(bucket, key, value):
-    s3 = boto3.resource('s3')
+    """Save the provide dictionary to an S3 bucket and key"""
+    s3 = boto3.resource("s3")
     obj = s3.Object(bucket, key)
     obj.put(Body=json.dumps(value))
-    
-github_repos = ['dacort/athena-query-stats', 'awslabs/athena-glue-service-logs', 'awslabs/athena-adobe-datafeed-splitter', 'dacort/demo-code', 'dacort/sample-code', 'dacort/syslog-to-athena', 'dacort/damons-data-lake']
-traffic_endpoints = ['popular/referrers', 'popular/paths', 'views', 'clones']
+
+
+github_repos = [
+    "dacort/athena-query-stats",
+    "awslabs/athena-glue-service-logs",
+    "awslabs/athena-adobe-datafeed-splitter",
+    "dacort/demo-code",
+    "dacort/sample-code",
+    "dacort/syslog-to-athena",
+    "dacort/damons-data-lake",
+    "dacort/metabase-athena-driver",
+]
+traffic_endpoints = ["popular/referrers", "popular/paths", "views", "clones"]
 
 for repo in github_repos:
     for endpoint in traffic_endpoints:
-        url = 'https://api.github.com/repos/' + repo + '/traffic/' + endpoint
-        headers = {'Authorization': "token " + get_secret()}
+        url = "https://api.github.com/repos/" + repo + "/traffic/" + endpoint
+        headers = {"Authorization": "token " + get_secret()}
         r = requests.get(url, headers=headers)
-        if(r.ok):
+        if r.ok:
             events = json.loads(r.text or r.content)
             # Only write out the file if we got any useful data
             if events:
                 today = datetime.datetime.now().strftime("%Y-%m-%d")
-                s3_key = '%s/%s/%s/%s.json' % (get_job_arg('prefix'), endpoint.replace('popular/', 'traffic/'), repo, today)
-                save_results(get_job_arg('bucket'), s3_key, events)
+                s3_key = "%s/%s/%s/%s.json" % (
+                    get_job_arg("prefix"),
+                    endpoint.replace("popular/", "traffic/"),
+                    repo,
+                    today,
+                )
+                save_results(get_job_arg("bucket"), s3_key, events)
         else:
             # TODO: Error handling :)
             print(r)
