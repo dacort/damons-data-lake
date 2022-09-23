73a4fc3edd64df361248c7b71ba447c8d28cf87e
dacort <d.lifehacker@gmail.com>
2022-09-23 12:57:02 -0700

    Upgrade to CDKv2

--- data_containers/app.py
+++ data_containers/app.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import os
-
-from aws_cdk import core as cdk
+import aws_cdk as cdk
 
 from data_containers.data_containers_stack import DataContainersStack, GlueStack
-
 
 app = cdk.App()
 DataContainersStack(app, "DataContainersStack")
